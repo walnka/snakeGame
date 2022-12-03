df203d65d15ee10bdb686af2c78052e748b584c0
walnka <willemdesu@gmail.com>
2022-12-03 02:28:56 -0800

    Implemented server and client sockets with comments

--- part2_client.py
+++ part2_client.py
@@ -2,17 +2,25 @@ from socket import *
 import time
 
 # Define Constants
-receiveTimeout = 1 #ms
+receiveTimeout = 1 #s
+numPackets = 5 # Number of packets sent
+
 # Set Up Local Host connection as client
 serverName = "localhost"
 serverPort=12000
-clientSocket=socket(AF_INET,SOCK_DGRAM)
+clientSocket=socket(AF_INET,SOCK_DGRAM) # setup as UDP socket protocol
+clientSocket.settimeout(receiveTimeout) # set timeout of socket commands to 1s
 
-
-message=input("Input lowercase sentence:")
-clientSocket.sendto(message.encode(),(serverName,serverPort))
-packetSentTime = time.time()
-while (time.time() - packetSentTime < receiveTimeout):
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    print(modifiedMessage.decode())
-clientSocket.close()
+# Send numPackets amount of packets to the server
+for packetNum in range(numPackets):
+    message = f"PING {packetNum+1} - hello world" # message to send
+    clientSocket.sendto(message.encode(),(serverName,serverPort)) # encode and send the message to the server
+    packetSentTime = time.time() # store the time that the packet was sent
+    # try block to deal with timout if packet is dropped
+    try:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048) # receive message
+        rtt = round((time.time() - packetSentTime)*1000,3) # calculate RTT in ms and round to neares us
+        print(modifiedMessage.decode(), ": time = ", rtt, "ms") # print received message and RTT
+    except:
+        print(f"Request for packet {packetNum+1} timed out") # Timeout error message if packet was dropped
+clientSocket.close() # Close socket
--- part2_server.py
+++ part2_server.py
@@ -5,17 +5,18 @@ import time
 # Set Up Local Host connection as server
 serverName = "localhost"
 serverPort=12000
-serverSocket=socket(AF_INET,SOCK_DGRAM)
+serverSocket=socket(AF_INET,SOCK_DGRAM) # setup as UDP socket protocol
 serverSocket.bind(('',serverPort))
 print("The server is ready to receive")
 
+# while the server is running wait to receive messages
 while True:
     # Recieve message
-    message, clientAddress = serverSocket.recvfrom(2048)
+    message, clientAddress = serverSocket.recvfrom(2048) # receive up to 2048bytes from client
     # Generate random 10% chance that message is lost
-    if ( 10%10 != 0):
+    if ( round(random.uniform(1,10),0)%10 != 0):
         # Delay for random time between 5ms-50ms
         time.sleep(round(random.uniform(.005, .05), 3))
-        modifiedMessage = message.decode().upper()
-        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+        modifiedMessage = message.decode().replace("hello world", "ditto") # decod and replace hello world with ditto
+        serverSocket.sendto(modifiedMessage.encode(), clientAddress) # encode message and send back to same client address that sent it
     
