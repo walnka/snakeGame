aae52d4b5981910187efe8f13745dc2b79001d58
walnka <willemdesu@gmail.com>
2022-12-02 19:35:24 -0800

    Fixed randomizaiton of server side

--- part2_client.py
+++ part2_client.py
@@ -1,10 +1,18 @@
 from socket import *
+import time
 
-serverName = "hostname"
+# Define Constants
+receiveTimeout = 1 #ms
+# Set Up Local Host connection as client
+serverName = "localhost"
 serverPort=12000
 clientSocket=socket(AF_INET,SOCK_DGRAM)
+
+
 message=input("Input lowercase sentence:")
 clientSocket.sendto(message.encode(),(serverName,serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+packetSentTime = time.time()
+while (time.time() - packetSentTime < receiveTimeout):
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print(modifiedMessage.decode())
 clientSocket.close()
--- part2_server.py
+++ part2_server.py
@@ -1,11 +1,21 @@
 from socket import *
+import random
+import time
 
-serverName = "hostname"
+# Set Up Local Host connection as server
+serverName = "localhost"
 serverPort=12000
 serverSocket=socket(AF_INET,SOCK_DGRAM)
 serverSocket.bind(('',serverPort))
 print("The server is ready to receive")
+
 while True:
+    # Recieve message
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # Generate random 10% chance that message is lost
+    if ( 10%10 != 0):
+        # Delay for random time between 5ms-50ms
+        time.sleep(round(random.uniform(.005, .05), 3))
+        modifiedMessage = message.decode().upper()
+        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    
